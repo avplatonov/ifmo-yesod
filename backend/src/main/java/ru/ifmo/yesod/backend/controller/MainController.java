4b945b2dc377f5ab22b2be6870c7474d2beb7e70
Alexey Platonov <aplatonovv@gmail.com>
2019-08-22 18:39:35 +0300

    fix parent

--- backend/src/main/java/ru/ifmo/yesod/backend/controller/MainController.java
+++ backend/src/main/java/ru/ifmo/yesod/backend/controller/MainController.java
@@ -3,17 +3,13 @@ package ru.ifmo.yesod.backend.controller;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
- 
+
 import ru.ifmo.yesod.backend.model.DocumentItem;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-
-
-
-
 
 @Controller
 public class MainController {
@@ -31,24 +27,26 @@ public class MainController {
     	results.add(new DocumentItem(" Соглашение между Правительством Российской Федерации и Правительством Соединенного Королевства Великобритании",0.3,0.5));
     	results.add(new DocumentItem("СОГЛАШЕНИЕ  между Правительством Союза Советских Социалистических   Республик и Правительством Соединенного Королевства",0.4,0.2));
     	results.add(new DocumentItem("Соглашение между Правительством Российской Федерации и  Правительством Социалистической Республики Вьетнам",0.7,0.4));
-    	
+
     }
-    
-    
+
+
     @RequestMapping(value = { "/", "/index" }, method = RequestMethod.GET)
-    public String index(Model model, @RequestParam(value="query",required = false) String text, @RequestParam(value="sort",required = false) String sortType) {
-    	
+    public String index(Model model,
+		@RequestParam(value="query",required = false) String text,
+		@RequestParam(value="sort",required = false) String sortType
+	) {
     	if (text == null || text == ""){
     		return "index";
-    	}    	
+    	}
     	else {
     		model.addAttribute("query", text);
-    		if(sortType == null || sortType.equals("1") ) {    			
-    	    	Collections.sort(results, DocumentItem.bellCompare); 
+    		if(sortType == null || sortType.equals("1") ) {
+    	    	Collections.sort(results, DocumentItem.bellCompare);
     			model.addAttribute("results", results );
     		}
     		else if (sortType.equals("0")){
-    			Collections.sort(results, DocumentItem.concurrenceCompare); 
+    			Collections.sort(results, DocumentItem.concurrenceCompare);
     			model.addAttribute("results", results );
     		}
     		return "response";
--- backend/src/main/java/ru/ifmo/yesod/backend/model/DocumentItem.java
+++ backend/src/main/java/ru/ifmo/yesod/backend/model/DocumentItem.java
@@ -2,13 +2,11 @@ package ru.ifmo.yesod.backend.model;
 
 import java.util.Comparator;
 
-
-
 public class DocumentItem {
 	private String title;
 	private double pointsBell;
 	private double pontsConcurrence;
-	
+
 	public DocumentItem(String title, double pointsBell, double pontsConcurrence) {
 		this.title = title;
 		this.pointsBell = pointsBell;
@@ -38,26 +36,26 @@ public class DocumentItem {
 	public void setPontsConcurrence(double pontsConcurrence) {
 		this.pontsConcurrence = pontsConcurrence;
 	}
-	
-	
+
+
 	public static Comparator<DocumentItem> bellCompare = new Comparator<DocumentItem>() {
 		public int compare(DocumentItem r1, DocumentItem r2) {
 			double point1 = r1.getPointsBell();
 			double point2 = r2.getPointsBell();
-			
+
 			return (int) ((point1 - point2)*10);
 		}
 	};
-	
+
 	public static Comparator<DocumentItem> concurrenceCompare = new Comparator<DocumentItem>() {
 		public int compare(DocumentItem r1, DocumentItem r2) {
 			double point1 = r1.getPontsConcurrence();
 			double point2 = r2.getPontsConcurrence();
-			
+
 			return (int) ((point1 - point2)*10);
 		}
 	};
-	
+
 }
-	
+
 
